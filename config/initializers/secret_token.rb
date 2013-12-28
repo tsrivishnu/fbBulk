146d0f88f3cbf3f455ab2019179191737ceff74d
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2013-12-29 01:14:00 +0530

    initial commit  with working code for saving the users access token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FbBulk::Application.config.secret_key_base = '6c244b2a06e01dca1ffe6c3db466866d74627da324aa9258792ae1b239c97aab84ff70a354ca4088b02c7a70eb5dfe0eefa6714a9dd62b274e2f2134789fff66'
